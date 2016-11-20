f8f04462adfeb573752ad40024f95ff887f758d3
Andy Hebrank <ahebrank@gmail.com>
2016-11-20 17:05:11 -0500

    fix wsgi entry

--- endpoint/wsgi.py
+++ endpoint/wsgi.py
@@ -1,7 +1,8 @@
 import os
 from issue_proxy import create_app
 
+config = os.path.dirname(os.path.realpath(__file__)) + '/repos.json'
+app = create_app(config)
+
 if __name__ == "__main__":
-	config = os.path.dirname(os.path.realpath(__file__)) + '/repos.json'
-	app = create_app(config)
 	app.run()

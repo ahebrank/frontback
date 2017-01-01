6f328bd4bc9d7afac30dc5315436e18f1da400c8
Andy Hebrank <ahebrank@gmail.com>
2017-01-01 12:53:43 -0500

    mod_wsgi compatibility

--- endpoint/wsgi.py
+++ endpoint/wsgi.py
@@ -2,7 +2,7 @@ import os
 from issue_proxy import create_app
 
 config = os.path.dirname(os.path.realpath(__file__)) + '/repos.json'
-app = create_app(config)
+application = create_app(config)
 
 if __name__ == "__main__":
-	app.run()
+	application.run()

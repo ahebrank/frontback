49365f62d28dcdc8acbdda1088770695fb70222d
Andy Hebrank <ahebrank@gmail.com>
2019-02-21 14:01:42 -0500

    fix #41: swap in placeholders for remote origin objects

---
+++ src/js/lib/paste.js
@@ -0,0 +1,22 @@
+module.exports = {
+  on: function($) {
+    $('#ftbk-feedback-overview').on('paste', function(event) {
+      var items = (event.clipboardData || event.originalEvent.clipboardData).items;
+      for (index in items) {
+          var item = items[index];
+          if (item.kind === 'file') {
+          var blob = item.getAsFile();
+          var reader = new FileReader();
+          reader.onload = function(event){
+              $('#ftbk-feedback-screenshot').attr('src', event.target.result);
+          }; 
+          reader.readAsDataURL(blob);
+          }
+      }
+    });
+  },
+
+  off: function($) {
+    this.$('#ftbk-feedback-overview').off('paste');
+  }
+}
